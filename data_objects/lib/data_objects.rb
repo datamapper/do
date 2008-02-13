fdaff907502c03991ce7b73bfa94c2ef4ab3e3f1
Sam Smoot <ssmoot@gmail.com>
2008-02-12 18:50:16 -0600

    Moving the quoting to it's own module, finished up the Command spec I think.

--- data_objects/lib/data_objects.rb
+++ data_objects/lib/data_objects.rb
@@ -3,3 +3,21 @@ require File.dirname(__FILE__) + "/transaction"
 require File.dirname(__FILE__) + "/command"
 require File.dirname(__FILE__) + "/result_data"
 require File.dirname(__FILE__) + "/reader"
+
+
+
+# class NotImplementedError < StandardError; end
+# 
+# class ConnectionFailed < StandardError; end
+# 
+# class ReaderClosed < StandardError; end
+# 
+# class ReaderError < StandardError; end
+# 
+# class QueryError < StandardError; end
+# 
+# class NoInsertError < StandardError; end
+# 
+# class LostConnectionError < StandardError; end
+# 
+# class UnknownError < StandardError; end
--- data_objects/spec/command_spec.rb
+++ data_objects/spec/command_spec.rb
@@ -6,14 +6,15 @@ describe DataObjects::Command do
     connection = DataObjects::Connection.new('mock://localhost')
     
     command = connection.create_command("SELECT * FROM example")
-    
+
+    command.should respond_to(:connection)
     command.should respond_to(:to_s)
     
     command.should respond_to(:execute_non_query)
     command.should respond_to(:execute_reader)
     
     command.should respond_to(:set_types)
-    p command.class
+    
     connection.close
   end
   
