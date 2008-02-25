d5fbf011a38e704523b5a88cef3dd5d1777e31c5
Bernerd Schaefer <bj.schaefer@gmail.com>
2008-02-25 17:26:20 -0600

    Faster DateTime parsing, extracted some date computing code

--- do_sqlite3/ext/rbsqlite3.c
+++ do_sqlite3/ext/rbsqlite3.c
@@ -139,7 +139,7 @@ VALUE native_typecast(sqlite3_value *value, int type) {
 			break;
 		}
 		case SQLITE3_TEXT: {
-			ruby_value = rb_str_new2(sqlite3_value_text(value));
+			ruby_value = rb_str_new2((char*)sqlite3_value_text(value));
 			break;
 		}
 		case SQLITE_FLOAT: {
@@ -150,6 +150,31 @@ VALUE native_typecast(sqlite3_value *value, int type) {
 	return ruby_value;
 }
 
+// Find the greatest common denominator and reduce the provided numerator and denominator.
+// This replaces calles to Rational.reduce! which does the same thing, but really slowly.
+void reduce( unsigned long long int *numerator, unsigned long long int *denominator ) {
+	unsigned long long int a, b, c;
+	a = *numerator;
+	b = *denominator;
+	while ( a != 0 ) {
+		c = a; a = b % a; b = c;
+	}
+	*numerator = *numerator / b;
+	*denominator = *denominator / b;
+}
+
+// Generate the date integer which Date.civil_to_jd returns
+int jd_from_date(int year, int month, int day) {
+	int a, b;
+	if ( month <= 2 ) {
+		year -= 1;
+		month += 12;
+	}
+	a = year / 100;
+	b = 2 - a + (a / 4);
+	return floor(365.25 * (year + 4716)) + floor(30.6001 * (month + 1)) + day + b - 1524;
+}
+
 // Add rescue handling for null, etc.
 VALUE ruby_typecast(sqlite3_value *value, char *type) {
 	VALUE ruby_value = Qnil;
@@ -157,73 +182,61 @@ VALUE ruby_typecast(sqlite3_value *value, char *type) {
 		ruby_value = INT2NUM(sqlite3_value_int(value));
 	}
 	else if ( strcmp(type, "String") == 0 ) {
-		ruby_value = rb_str_new2(sqlite3_value_text(value));
+		ruby_value = rb_str_new2((char*)sqlite3_value_text(value));
 	}
 	else if ( strcmp(type, "Float") == 0 ) {
 		ruby_value = rb_float_new(sqlite3_value_double(value));
 	}
 	else if ( strcmp(type, "Date") == 0 ) {
 		int year, month, day;
-		char *date = sqlite3_value_text(value);
-		
-		// Used by math pulled out of Date.civil_to_jd and jd_to_ajd
-		int a, b, jd, ajd;
+		char *date = (char*)sqlite3_value_text(value);
+		int jd, ajd;
 		VALUE rational;
 		
 		sscanf(date, "%4d-%2d-%2d", &year, &month, &day);
 		
-		// Math from Date.civil_to_jd
-		if ( month <= 2 ) {
-			year -= 1;
-			month += 12;
-		}
-		a = year / 100;
-		b = 2 - a + (a / 4);
-		jd = floor(365.25 * (year + 4716)) + floor(30.6001 * (month + 1)) + day + b - 1524;
+		jd = jd_from_date(year, month, day);
 		
 		// Math from Date.jd_to_ajd
 		ajd = jd * 2 - 1;
 		rational = rb_funcall(rb_cRational, rb_intern("new!"), 2, INT2NUM(ajd), INT2NUM(2));
-		
-		// Original (slooooow) Date [~5.8 seconds / 1000.times]:
-		//		ruby_value = rb_funcall(rb_cDate, ID_PARSE, 1, rb_str_new2(sqlite3_value_text(value)));
-		// Faster Date [~2.2 seconds / 1000.times]: 
-		// 		ruby_value = rb_funcall(rb_cDate, ID_CIVIL, 3, INT2NUM(year), INT2NUM(month), INT2NUM(day));
-		
-		// Super fastest Date creation! [~0.25 seconds / 1000.times]
-		// Yeah, that's 23 times faster than Date.parse
 		ruby_value = rb_funcall(rb_cDate, rb_intern("new!"), 3, rational, INT2NUM(0), INT2NUM(2299161));
 	}
 	else if ( strcmp(type, "DateTime") == 0 ) {
-		int a, b, jd;
+		int jd;
 		int y, m, d, h, min, s;
-		char *date = sqlite3_value_text(value);
+		char *date = (char*)sqlite3_value_text(value);
 		
 		sscanf(date, "%4d-%2d-%2d %2d:%2d:%2d", &y, &m, &d, &h, &min, &s);
 		
-		// Original (slooooow) DateTime [~12 seconds / 1000.times]
-		// 		ruby_value = rb_funcall(rb_cDateTime, ID_PARSE, 1, rb_str_new2(sqlite3_value_text(value)));
+		jd = jd_from_date(y, m, d);
 		
-		// Faster DateTime [ ~7.3 seconds / 1000.times]
-		// 		ruby_value = rb_funcall(rb_cDateTime, ID_CIVIL, 6, INT2NUM(y), INT2NUM(m), INT2NUM(d), INT2NUM(h), INT2NUM(min), INT2NUM(s));
+		// Generate ajd with fractional days for the time
+		// Extracted from Date#jd_to_ajd, Date#day_fraction_to_time, and Rational#+ and #-
+		unsigned long long int num, den;
 		
-		// Somewhat Faster [~6.3 seconds / 1000.times ]
+		num = (h * 1440) + (min * 24);
+		den = (24 * 1440);
+		reduce(&num, &den);
 		
-		if ( m <= 2 ) {
-			y -= 1;
-			m += 12;
-		}
-		a = y / 100;
-		b = 2 - a + (a / 4);
-		jd = floor(365.25 * (y + 4716)) + floor(30.6001 * (m + 1)) + d + b - 1524;
+		num = (num * 86400) + (s * den);
+		den = den * 86400;
+		reduce(&num, &den);
 		
-		VALUE fraction = rb_funcall(rb_cDate, rb_intern("time_to_day_fraction"), 3, INT2NUM(h), INT2NUM(min), INT2NUM(s));
-		VALUE ajd = rb_funcall(rb_cDate, rb_intern("jd_to_ajd"), 2, INT2NUM(jd), fraction);
+		num = (jd * den) + num;
+		
+		num = num * 2;
+		num = num - den;
+		den = den * 2;
+		
+		reduce(&num, &den);
+		
+		VALUE ajd = rb_funcall(rb_cRational, rb_intern("new!"), 2, rb_ull2inum(num), rb_ull2inum(den));
 		ruby_value = rb_funcall(rb_cDateTime, rb_intern("new!"), 3, ajd, INT2NUM(0), INT2NUM(2299161));
 	}
 	else if ( strcmp(type, "Time") == 0 ) {
 		int y, m, d, h, min, s;
-		char *date = sqlite3_value_text(value);
+		char *date = (char*)sqlite3_value_text(value);
 		
 		sscanf(date, "%4d-%2d-%2d %2d:%2d:%2d", &y, &m, &d, &h, &min, &s);
 		
