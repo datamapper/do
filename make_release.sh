06210c143d892dc137979c0d23c4c89e4f3a341b
Paul Sadauskas <pjs@absolute-performance.com>
2008-03-13 11:20:14 -0600

    Fix creating pkg dir & git ignore it

--- make_release.sh
+++ make_release.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
+mkdir -p pkg
 
 cd data_objects
 rake package
-cp pkg/*.gem ../pkg
+cp pkg/*.gem ../pkg/
 cd ..
 
 cd do_mysql
 rake package
-cp pkg/*.gem ../pkg
+cp pkg/*.gem ../pkg/
 cd ..
 
 cd do_sqlite3
 rake package
-cp pkg/*.gem ../pkg
+cp pkg/*.gem ../pkg/
 cd ..
 
 cd do_postgres
 rake package
-cp pkg/*.gem ../pkg
+cp pkg/*.gem ../pkg/
 cd ..
