c68b09bf61962b9fb17ad0d9c47ba799ee1f24cb
Paul Sadauskas <pjs@absolute-performance.com>
2008-03-17 14:27:39 -0600

    Make affected_rows accessible

--- data_objects/lib/result.rb
+++ data_objects/lib/result.rb
@@ -1,14 +1,11 @@
 module DataObjects
   class Result
+    attr_accessor :insert_id, :affected_rows
   
     def initialize(command, affected_rows, insert_id = nil)
       @command, @affected_rows, @insert_id = command, affected_rows, insert_id
     end
 
-    def insert_id
-      @insert_id
-    end
-    
     def to_i
       @affected_rows
     end
