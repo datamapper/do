126f22a632aad784f5a2d420555ac07aa7a22892
david <dgleal@gmail.com>
2008-04-11 22:43:21 +0100

    Initial import. Not usable yet.

---
+++ do_jdbc/spec/do_jdbc_spec.rb
@@ -0,0 +1,79 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe DataObjects::Jdbc do
+  it "should expose the proper DataObjects classes" do
+    DataObjects::Jdbc.const_get('Connection').should_not be_nil
+    DataObjects::Jdbc.const_get('Command').should_not be_nil
+    DataObjects::Jdbc.const_get('Result').should_not be_nil
+    DataObjects::Jdbc.const_get('Reader').should_not be_nil
+  end
+  
+  it "should connect successfully using the full URI" do
+    DataObjects::Jdbc::Connection.
+      new(URI.parse("jdbc://postgres:pg123@localhost:5432/do_jdbc_test?driver=org.postgresql.Driver&protocol=postgresql"))
+  end
+end
+
+describe DataObjects::Jdbc::Connection do
+  before(:each) do
+    # Open a connection for the specs to work with
+    @connection = DataObjects::Jdbc::Connection.new("jdbc://postgres:pg123@localhost:5432/do_jdbc_test?driver=org.postgresql.Driver&protocol=postgresql")
+  end
+
+  it "should be able to create a command" do
+    command = @connection.create_command("SELECT * FROM widgets")
+    command.should be_kind_of(DataObjects::Jdbc::Command)
+  end
+
+  describe "executing a query" do
+    before(:each) do
+      @command = @connection.create_command("SELECT * FROM widgets LIMIT 2")
+    end
+
+    describe "reading results" do
+      before(:each) do
+	@reader = @command.execute_reader
+      end
+  
+      it "should return the proper number of fields" do
+	@reader.fields.size.should == 18
+      end
+
+      it "should fetch 2 rows" do
+	@reader.next!.should == true
+	@reader.values.should be_kind_of(Array)
+
+	@reader.next!.should == true
+	@reader.values.should be_kind_of(Array)
+        
+	@reader.next!.should be_nil
+      end
+    end
+
+    describe "executing a query w/ set_types" do      
+      before(:all) do
+        @types = [
+          Fixnum, String, String, String, String, String,
+          String, Fixnum, Fixnum, Fixnum, Fixnum, Float, Float, 
+	  BigDecimal, Date, DateTime, DateTime, String
+        ]
+      end
+
+      before(:each) do
+        @command = @connection.create_command("SELECT * FROM widgets LIMIT 2")
+        @command.set_types @types
+        @reader = @command.execute_reader
+      end
+
+      # HACK: This seems like a weak test
+      it "should typecast all fields to the proper Ruby type" do
+        @reader.next!
+
+        @types.each_with_index do |t, idx|
+          @reader.values[idx].class.should == @types[idx]
+        end
+      end
+
+    end
+  end
+end
---
+++ do_jdbc/spec/spec_helper.rb
@@ -0,0 +1,8 @@
+$TESTING=true
+$:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+$:.unshift File.join(File.dirname(__FILE__), '..', '..', 'data_objects', 'lib')
+
+require 'rubygems'
+require 'data_objects'
+require 'date'
+require 'do_jdbc'
