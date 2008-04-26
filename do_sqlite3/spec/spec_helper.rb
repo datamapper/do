22fb692ced0933eb9583b3ef255fc3b956d60a32
Luis Lavena <luislavena@gmail.com>
2008-04-26 09:32:04 -0300

    Do not use installed data_object gems.
    The correct way is use the data_objects library from the repository.
    Also relocate the place where specs will look for do_sqlite3.

--- do_sqlite3/spec/spec_helper.rb
+++ do_sqlite3/spec/spec_helper.rb
@@ -2,7 +2,14 @@ $TESTING=true
 
 require 'rubygems'
 require 'spec'
-require 'data_objects'
 require 'date'
-require File.join(File.dirname(__FILE__), "..", "do_sqlite3")
+
+# push data_objects from repository in the load path
+# DO NOT USE installed gem of data_objects!
+$:.push File.join(File.dirname(__FILE__), '../../data_objects', 'lib')
+require 'data_objects'
+
+# put the pre-compiled extension in the path to be found
+$:.push File.join(File.dirname(__FILE__), '..', 'lib')
+require 'do_sqlite3'
 
