98690178419dcca4beaf947f3a873b2c600315fa
Luis Lavena <luislavena@gmail.com>
2008-04-26 18:54:12 -0300

    Corrected DataObject and do_mysql dependency loading.

--- do_mysql/spec/spec_helper.rb
+++ do_mysql/spec/spec_helper.rb
@@ -2,6 +2,12 @@ $TESTING=true
 
 require 'rubygems'
 require 'spec'
-require 'data_objects'
 require 'date'
-require File.join(File.dirname(__FILE__), "..", "do_mysql")
+# push data_objects from repository in the load path
+# DO NOT USE installed gem of data_objects!
+$:.push File.join(File.dirname(__FILE__), '../../data_objects', 'lib')
+require 'data_objects'
+
+# put the pre-compiled extension in the path to be found
+$:.push File.join(File.dirname(__FILE__), '..', 'lib')
+require 'do_mysql'
