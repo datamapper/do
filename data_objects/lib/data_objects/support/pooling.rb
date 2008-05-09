192139cc50d370ecb59fbffd5c85b3061b5cdb70
Michael S. Klishin <michael@novemberain.com>
2008-05-09 21:20:23 +0300

    Some spec examples, more to come.

--- data_objects/lib/data_objects/support/pooling.rb
+++ data_objects/lib/data_objects/support/pooling.rb
@@ -19,6 +19,63 @@ class Object
   # Pool obviously has to be thread safe because state of
   # object is reset when it is released.
   module Pooling
+    def self.included(base)
+      base.send(:extend, ClassMethods)
+    end
 
+    module ClassMethods
+      def flush!
+      end
+    end
+
+    class DoesNotRespondToDispose < ArgumentError
+      def initialize(klass)
+        super "Class #{klass} must implement dispose instance method to be poolable."
+      end
+    end
+
+    class ResourcePool
+      attr_reader :size_limit, :size, :reserved, :available, :class_of_resources
+
+      def initialize(size_limit, class_of_resources)
+        raise ArgumentError.new("Expected class of resources to be instance of Class") unless class_of_resources.is_a?(Class)
+        raise ArgumentError.new("Class #{class_of_resources} must implement dispose instance method to be poolable.") unless class_of_resources.instance_methods.include?("dispose")
+
+        @size_limit         = size_limit
+        @class_of_resources = class_of_resources
+
+        @reserved  = Set.new
+        @available = Set.new
+      end
+
+      def size
+        0
+      end
+
+      def size_limit_hit?
+      end
+
+      def aquire
+      end
+    end # ResourcePool
+
+    class ResourcePoolWithEnvironment < ResourcePool
+      attr_reader :class_of_environment
+
+      def initialize(size_limit, class_of_resources, env_class)
+        super(size_limit, class_of_resources)
+        raise ArgumentError.new("Class #{class_of_resources} must implement aquire and release instance methods to be usable as resource environment.") unless valid_environment_class?(env_class)
+
+        @class_of_environment = env_class
+      end
+
+      protected
+
+      def valid_environment_class?(klass)
+        instance_methods_list = klass.instance_methods
+
+        instance_methods_list.include?("aquire") && instance_methods_list.include?("release")
+      end
+    end # ResourcePoolWithEnvironment
   end
 end
--- data_objects/spec/data_objects/support/pooling_spec.rb
+++ data_objects/spec/data_objects/support/pooling_spec.rb
@@ -3,6 +3,7 @@ require 'timeout'
 
 
 class SomeResource
+  include Object::Pooling
   attr_reader :name
 
   def initialize(name)
@@ -14,9 +15,19 @@ class SomeResource
   end
 end
 
+class UndisposableResource
+end
+
+
 
 describe Object::Pooling do
-  it "provides instance method for flushing the pool"
+  before :each do
+    @air = SomeResource.new("air")
+  end
+
+  it "provides instance method for flushing the pool" do
+    SomeResource.should respond_to(:flush!)
+  end
 
   it "provides access to the pool"
 end
@@ -25,30 +36,87 @@ end
 
 describe Object::Pooling::ResourcePool do
   before :each do
-    @pool = Object::Pooling::ResourcePool.new
+    @pool = Object::Pooling::ResourcePool.new(7, SomeResource)
   end
 
   it "responds to aquire" do
     @pool.should respond_to(:aquire)
   end
 
-  it "has a size limit"
+  it "has a size limit" do
+    @pool.size_limit.should == 7
+  end
 
-  it "has current size"
+  it "has initial size of zero" do
+    @pool.size.should == 0
+  end
 
-  it "has a readable set of reserved resources"
+  it "has a readable set of reserved resources" do
+    @pool.reserved.should be_empty
+  end
 
-  it "has a readable set of available resources"
+  it "has a readable set of available resources" do
+    @pool.available.should be_empty
+  end
 
-  it "knows whether it has available resources left"
+  it "knows whether it has available resources left" do
+    @pool.should respond_to(:size_limit_hit?)
+  end
 
-  it "knows class of resources (objects) it works with"
+  it "knows class of resources (objects) it works with" do
+    @pool.class_of_resources.should == SomeResource
+  end
 
-  it "requires class of resources (objects) it works with to have a dispose instance method"
+  it "raises exception when given anything but class for resources class" do
+    lambda { @pool = Object::Pooling::ResourcePool.new(7, "Hooray!") }.should raise_error(ArgumentError, /class/)
+  end
 
-  it "has a resource environment"
+  it "requires class of resources (objects) it works with to have a dispose instance method" do
+    lambda { @pool = Object::Pooling::ResourcePool.new(3, UndisposableResource) }.should raise_error(ArgumentError, /dispose/)
+  end
+end
 
-  it "assumes resource environment responds to aquire"
 
-  it "assumes resource environment responds to release"
+
+class Water
+  include Object::Pooling
+
+  def dispose
+  end
+end
+
+class PlanetEarthEnvironment
+  def aquire
+  end
+
+  def release
+  end
+end
+
+class EnvironmentWithoutAquire
+  def release
+  end
+end
+
+class EnvironmentWithoutRelease
+  def aquire
+  end
+end
+
+describe Object::Pooling::ResourcePoolWithEnvironment do
+  before :each do
+    @pool         = Object::Pooling::ResourcePoolWithEnvironment.new(3, Water, PlanetEarthEnvironment)
+  end
+
+  it "has a resource environment" do
+    @pool.class_of_environment.should == PlanetEarthEnvironment
+  end
+
+  it "assumes resource environment responds to aquire" do
+    lambda { @pool = Object::Pooling::ResourcePoolWithEnvironment.new(3, Water, EnvironmentWithoutAquire) }.should raise_error(ArgumentError, /aquire/)
+  end
+
+  it "assumes resource environment responds to release" do
+    lambda { @pool = Object::Pooling::ResourcePoolWithEnvironment.new(3, Water, EnvironmentWithoutRelease) }.should raise_error(ArgumentError, /release/)
+  end
 end
