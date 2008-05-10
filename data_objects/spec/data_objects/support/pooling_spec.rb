d3d16093284a5f4e5952318a035f62d657ee76d4
Michael S. Klishin <michael@novemberain.com>
2008-05-10 15:56:10 +0300

    Renamed SomeResource => DisposableResource in pooling spec.

--- data_objects/spec/data_objects/support/pooling_spec.rb
+++ data_objects/spec/data_objects/support/pooling_spec.rb
@@ -2,7 +2,7 @@ require File.join(File.dirname(__FILE__), '..', '..', '..', 'lib', 'data_objects
 require 'timeout'
 
 
-class SomeResource
+class DisposableResource
   include Object::Pooling
   attr_reader :name
 
@@ -22,7 +22,7 @@ end
 
 describe Object::Pooling::ResourcePool do
   before :each do
-    @pool = Object::Pooling::ResourcePool.new(7, SomeResource)
+    @pool = Object::Pooling::ResourcePool.new(7, DisposableResource)
   end
 
   it "responds to flush!" do
@@ -54,7 +54,7 @@ describe Object::Pooling::ResourcePool do
   end
 
   it "knows class of resources (objects) it works with" do
-    @pool.class_of_resources.should == SomeResource
+    @pool.class_of_resources.should == DisposableResource
   end
 
   it "raises exception when given anything but class for resources class" do
@@ -71,45 +71,45 @@ end
 
 describe "Aquire from contant size pool" do
   before :each do
-    SomeResource.initialize_pool(2)
+    DisposableResource.initialize_pool(2)
   end
 
   it "places initialized instances to the pool" do
-    @time = SomeResource.pool.aquire
-    SomeResource.pool.size.should == 1
+    @time = DisposableResource.pool.aquire
+    DisposableResource.pool.size.should == 1
   end
 
   it "places initialized instance in the reserved set" do
-    @time = SomeResource.pool.aquire
-    SomeResource.pool.reserved.size.should == 1
+    @time = DisposableResource.pool.aquire
+    DisposableResource.pool.reserved.size.should == 1
   end
 
   it "raises an exception when pool size limit is hit" do
-    @t1 = SomeResource.pool.aquire
-    @t2 = SomeResource.pool.aquire
+    @t1 = DisposableResource.pool.aquire
+    @t2 = DisposableResource.pool.aquire
 
-    lambda { SomeResource.pool.aquire }.should raise_error(RuntimeError)
+    lambda { DisposableResource.pool.aquire }.should raise_error(RuntimeError)
   end
 
   it "returns last released resource" do
-    @t1 = SomeResource.pool.aquire
-    @t2 = SomeResource.pool.aquire
-    SomeResource.pool.release(@t1)
+    @t1 = DisposableResource.pool.aquire
+    @t2 = DisposableResource.pool.aquire
+    DisposableResource.pool.release(@t1)
 
-    SomeResource.pool.aquire.should == @t1
+    DisposableResource.pool.aquire.should == @t1
   end
 
   it "really truly returns last released resource" do
-    @t1 = SomeResource.pool.aquire
-    SomeResource.pool.release(@t1)
+    @t1 = DisposableResource.pool.aquire
+    DisposableResource.pool.release(@t1)
 
-    @t2 = SomeResource.pool.aquire
-    SomeResource.pool.release(@t2)
+    @t2 = DisposableResource.pool.aquire
+    DisposableResource.pool.release(@t2)
 
-    @t3 = SomeResource.pool.aquire
-    SomeResource.pool.release(@t3)
+    @t3 = DisposableResource.pool.aquire
+    DisposableResource.pool.release(@t3)
 
-    SomeResource.pool.aquire.should == 1
+    DisposableResource.pool.aquire.should == 1
     @t1.should == @t3
   end
 end
@@ -118,42 +118,42 @@ end
 
 describe "Releasing from contant size pool" do
   before :each do
-    SomeResource.initialize_pool(2)
+    DisposableResource.initialize_pool(2)
   end
 
   it "decreases size of the pool" do
-    @t1 = SomeResource.pool.aquire
-    @t2 = SomeResource.pool.aquire
-    SomeResource.pool.release(@t1)
+    @t1 = DisposableResource.pool.aquire
+    @t2 = DisposableResource.pool.aquire
+    DisposableResource.pool.release(@t1)
 
-    SomeResource.pool.size.should == 1
+    DisposableResource.pool.size.should == 1
   end
 
   it "raises an exception on attempt to releases object not in pool" do
-    @t1 = SomeResource.pool.aquire
-    @t2 = SomeResource.new
+    @t1 = DisposableResource.pool.aquire
+    @t2 = DisposableResource.new
 
-    SomeResource.pool.release(@t1)
-    lambda { SomeResource.pool.release(@t2) }.should raise_error(RuntimeError)
+    DisposableResource.pool.release(@t1)
+    lambda { DisposableResource.pool.release(@t2) }.should raise_error(RuntimeError)
   end
 
   it "disposes released object" do
-    @t1 = SomeResource.pool.aquire
+    @t1 = DisposableResource.pool.aquire
 
     @t1.should_receive(:dispose)
-    SomeResource.pool.release(@t1)
+    DisposableResource.pool.release(@t1)
   end
 
   it "removes released object from reserved set" do
-    @t1 = SomeResource.pool.aquire
+    @t1 = DisposableResource.pool.aquire
 
-    lambda { SomeResource.pool.release(@t1) }.should change(SomeResource.pool.reserved, :size).by(-1)
+    lambda { DisposableResource.pool.release(@t1) }.should change(DisposableResource.pool.reserved, :size).by(-1)
   end
 
   it "returns released object back to available set" do
-    @t1 = SomeResource.pool.aquire
+    @t1 = DisposableResource.pool.aquire
 
-    lambda { SomeResource.pool.release(@t1) }.should change(SomeResource.pool.available, :size).by(1)
+    lambda { DisposableResource.pool.release(@t1) }.should change(DisposableResource.pool.available, :size).by(1)
   end
 end
 
@@ -161,30 +161,30 @@ end
 
 describe "Flushing of contant size pool" do
   before :each do
-    SomeResource.initialize_pool(2)
+    DisposableResource.initialize_pool(2)
 
-    @t1 = SomeResource.pool.aquire
-    @t2 = SomeResource.pool.aquire
+    @t1 = DisposableResource.pool.aquire
+    @t2 = DisposableResource.pool.aquire
 
     # sanity check
-    SomeResource.pool.reserved.should_not be_empty
+    DisposableResource.pool.reserved.should_not be_empty
   end
 
   it "disposes all pooled objects" do
     [@t1, @t2].each { |instance| instance.should_receive(:dispose) }
 
-    SomeResource.pool.flush!
+    DisposableResource.pool.flush!
   end
 
   it "empties reserved set" do
-    SomeResource.pool.flush!
+    DisposableResource.pool.flush!
 
-    SomeResource.pool.reserved.should be_empty
+    DisposableResource.pool.reserved.should be_empty
   end
 
   it "returns all instances to available set" do
-    SomeResource.pool.flush!
+    DisposableResource.pool.flush!
 
-    SomeResource.pool.available.size.should == 2
+    DisposableResource.pool.available.size.should == 2
   end
 end
