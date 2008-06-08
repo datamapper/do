22d6c4857e7f23f0393e067a9165001d9689dcaf
Alex Coles <alexbcoles@mac.com>
2008-06-08 13:23:11 +0200

    Work on compilation workflow
    * buildr buildfile JAR's extension
    * Rakefile calls buildr to compile, and then copies JAR extension to lib/
    * Added unit test, spec_helper for do_derby
    * Renamed do_jdbc.rb to do_jdbc-support.rb

--- do_derby/lib/do_derby.rb
+++ do_derby/lib/do_derby.rb
@@ -1,8 +1,13 @@
+require 'rubygems'
+require 'data_objects'
+
 DERBY_VERSION = '10.4.1.3'
 
 if RUBY_PLATFORM =~ /java/
+  require 'do_jdbc-support'
+  require 'do_derby-ext-java'
   require 'java'
   require "derby-#{DERBY_VERSION}.jar"
 else
-  warn "jdbc-derby is only for use with JRuby"
+  warn "do-derby is only for use with JRuby"
 end
