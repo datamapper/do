6e1d0b828e5bc9a1f06417710fe810982815b4b2
Alex Coles <alexbcoles@mac.com>
2008-06-14 23:23:55 +0200

    Remove duplicated quoting specs

--- do_mysql/spec/integration/do_mysql_spec.rb
+++ do_mysql/spec/integration/do_mysql_spec.rb
@@ -89,40 +89,6 @@ describe DataObjects::Mysql::Connection do
 
 end
 
-describe "Quoting" do
-  include MysqlSpecHelpers
-
-  before :all do
-    setup_test_environment
-  end
-
-  it "should escape strings properly" do
-    command = @connection.create_command("SELECT * FROM widgets WHERE name = ?")
-    command.quote_string("Willy O'Hare & Johnny O'Toole").should == "'Willy O\\'Hare & Johnny O\\'Toole'".dup
-    command.quote_string("The\\Backslasher\\Rises\\Again").should == "'The\\\\Backslasher\\\\Rises\\\\Again'"
-    command.quote_string("Scott \"The Rage\" Bauer").should == "'Scott \\\"The Rage\\\" Bauer'"
-  end
-
-  it "should quote DateTime's properly" do
-    command = @connection.create_command("SELECT * FROM widgets WHERE release_datetime >= ?")
-    dt = DateTime.now
-    command.quote_datetime(dt).should == "'#{dt.strftime('%Y-%m-%d %H:%M:%S')}'"
-  end
-
-  it "should quote Time's properly" do
-    command = @connection.create_command("SELECT * FROM widgets WHERE release_timestamp >= ?")
-    dt = Time.now
-    command.quote_time(dt).should == "'#{dt.strftime('%Y-%m-%d %H:%M:%S')}'"
-  end
-
-  it "should quote Date's properly" do
-    command = @connection.create_command("SELECT * FROM widgets WHERE release_date >= ?")
-    dt = Date.today
-    command.quote_date(dt).should == "'#{dt.strftime('%Y-%m-%d')}'"
-  end
-
-end
-
 describe DataObjects::Mysql::Reader do
   include MysqlSpecHelpers
 
--- do_mysql/spec/integration/quoting_spec.rb
+++ do_mysql/spec/integration/quoting_spec.rb
@@ -1,16 +1,12 @@
 require 'pathname'
 require Pathname(__FILE__).dirname.expand_path.parent + 'spec_helper'
-
 require 'date'
 
-describe DataObjects::Mysql::Command do
+describe DataObjects::Mysql::Command. "Quoting" do
+  include MysqlSpecHelpers
 
-  before(:each) do
-    @connection = if JRUBY
-      DataObjects::Mysql::Connection.new(DO_MYSQL_SPEC_JDBC_URI)
-    else
-      DataObjects::Mysql::Connection.new(DO_MYSQL_SPEC_URI)
-    end
+  before :all do
+    setup_test_environment
   end
 
   it "should escape strings properly" do
@@ -20,19 +16,19 @@ describe DataObjects::Mysql::Command do
     command.quote_string("Scott \"The Rage\" Bauer").should == "'Scott \\\"The Rage\\\" Bauer'"
   end
 
-  it "should quote DateTime's properly" do
+  it "should quote DateTime instances properly" do
     command = @connection.create_command("SELECT * FROM widgets WHERE release_datetime >= ?")
     dt = DateTime.now
     command.quote_datetime(dt).should == "'#{dt.strftime('%Y-%m-%d %H:%M:%S')}'"
   end
 
-  it "should quote Time's properly" do
+  it "should quote Time instances properly" do
     command = @connection.create_command("SELECT * FROM widgets WHERE release_timestamp >= ?")
     dt = Time.now
     command.quote_time(dt).should == "'#{dt.strftime('%Y-%m-%d %H:%M:%S')}'"
   end
 
-  it "should quote Date's properly" do
+  it "should quote Date instances properly" do
     command = @connection.create_command("SELECT * FROM widgets WHERE release_date >= ?")
     dt = Date.today
     command.quote_date(dt).should == "'#{dt.strftime('%Y-%m-%d')}'"
