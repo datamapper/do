7c64edc0ba2a1365704fb3fe92e2ba021c14618a
Paul Lamb <paul@mbp17.local>
2008-08-27 09:38:06 -0500

    Updated buildr script, updated postgres jar to latest and changed sqlite to use the new maven repo.

--- jdbc_drivers/postgres/lib/do_jdbc/postgres_version.rb
+++ jdbc_drivers/postgres/lib/do_jdbc/postgres_version.rb
@@ -1,8 +1,8 @@
 module DataObjects
   module Jdbc
     module Postgres
-      VERSION  = '8.2'
-      JAR_NAME = "postgresql-#{DataObjects::Jdbc::Postgres::VERSION}-504.jdbc3.jar"
+      VERSION  = '8.3'
+      JAR_NAME = "postgresql-#{DataObjects::Jdbc::Postgres::VERSION}-603.jdbc3.jar"
     end
   end
 end
--- jdbc_drivers/sqlite3/lib/do_jdbc/sqlite3_version.rb
+++ jdbc_drivers/sqlite3/lib/do_jdbc/sqlite3_version.rb
@@ -1,8 +1,8 @@
 module DataObjects
   module Jdbc
     module SQLite3
-      VERSION  = '3.5.8'
-      JAR_NAME = "sqlite-#{DataObjects::Jdbc::SQLite3::VERSION}.jar"
+      VERSION  = '3.6.0'
+      JAR_NAME = "sqlite-jdbc-#{DataObjects::Jdbc::SQLite3::VERSION}.jar"
     end
   end
 end
