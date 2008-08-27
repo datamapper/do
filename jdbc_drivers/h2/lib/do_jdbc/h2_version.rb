4d880af6d3cd8087f587e43e116d102cacf02619
Paul Lamb <paul@mbp17.local>
2008-08-27 09:58:55 -0500

    Updated jdbc drivers.

--- jdbc_drivers/h2/lib/do_jdbc/h2_version.rb
+++ jdbc_drivers/h2/lib/do_jdbc/h2_version.rb
@@ -1,7 +1,7 @@
 module DataObjects
   module Jdbc
     module H2
-      VERSION  = '1.0.63'
+      VERSION  = '1.0.77'
       JAR_NAME = "h2-#{DataObjects::Jdbc::H2::VERSION}.jar"
     end
   end
--- jdbc_drivers/mysql/lib/do_jdbc/mysql_version.rb
+++ jdbc_drivers/mysql/lib/do_jdbc/mysql_version.rb
@@ -1,7 +1,7 @@
 module DataObjects
   module Jdbc
     module MySQL
-      VERSION  = '5.0.4'
+      VERSION  = '5.0.8'
       JAR_NAME = "mysql-connector-java-#{DataObjects::Jdbc::MySQL::VERSION}-bin.jar"
     end
   end
