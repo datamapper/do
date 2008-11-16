f48ff5abd6b38cbacb8467ccf9ba06838d1bfdd4
Dan Kubb <dan.kubb@autopilotmarketing.com>
2008-11-15 21:44:05 -0800

    Fixed to work with Addressable 2.0

--- data_objects/lib/data_objects/uri.rb
+++ data_objects/lib/data_objects/uri.rb
@@ -1,28 +1,28 @@
 require 'addressable/uri'
 
 module DataObjects
-  URI = Struct.new(:scheme, :user, :password, :host, :port, :specified_port, :path, :query, :fragment) 
+  URI = Struct.new(:scheme, :user, :password, :host, :port, :path, :query, :fragment)
 
   class URI
     def self.parse(uri)
       return uri if uri.kind_of?(self)
       uri = Addressable::URI::parse(uri) unless uri.kind_of?(Addressable::URI)
-      self.new(uri.scheme, uri.user, uri.password, uri.host, uri.port, uri.specified_port, uri.path, uri.query, uri.fragment)
+      self.new(uri.scheme, uri.user, uri.password, uri.host, uri.port, uri.path, uri.query, uri.fragment)
     end
 
     def to_s
       string = ""
-      string << "#{scheme}://"       if scheme
+      string << "#{scheme}://"   if scheme
       if user
         string << "#{user}"
-        string << ":#{password}"     if password
+        string << ":#{password}" if password
         string << "@"
       end
-      string << "#{host}"            if host
-      string << ":#{specified_port}" if specified_port
+      string << "#{host}"        if host
+      string << ":#{port}"       if port
       string << path.to_s
-      string << "?#{query}"          if query
-      string << "##{fragment}"       if fragment
+      string << "?#{query}"      if query
+      string << "##{fragment}"   if fragment
       string
     end
   end
