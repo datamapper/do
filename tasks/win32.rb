6d57fd83454b46319cb76a761d6531321a8ad8a9
Dan Kubb <dan.kubb@autopilotmarketing.com>
2008-11-27 15:46:43 -0800

    Updated rake clean to use .gitignore for the list of files to cleanup
    * Updated rake task to use the ruby version as a constant
    * Updated wget command to use auto-resume

--- tasks/win32.rb
+++ tasks/win32.rb
@@ -12,7 +12,7 @@
 def download_file(directory, file)
   when_writing "downloading #{file}" do
     cd(directory) do
-      system("wget #{file} || curl -C - -O #{file}")
+      system("wget -c #{file} || curl -C - -O #{file}")
     end
   end
 end
@@ -20,6 +20,8 @@ end
 # CCROOT = cross-compilation root
 STASH = "#{CCROOT}/stash"
 CROSS = "#{CCROOT}/cross"
+
+RUBY_VERSION = '1.8.6-p287'
 
 namespace :build do
   # contains downloaded files for cross compilation
@@ -37,32 +39,32 @@ namespace :build do
     task :ruby => "#{CROSS}/bin/ruby.exe"
 
     # download ruby
-    file "#{STASH}/ruby-1.8.6-p287.tar.gz" => STASH do
-      download_file(STASH, "ftp://ftp.ruby-lang.org/pub/ruby/1.8/ruby-1.8.6-p287.tar.gz")
+    file "#{STASH}/ruby-#{RUBY_VERSION}.tar.gz" => STASH do
+      download_file(STASH, "ftp://ftp.ruby-lang.org/pub/ruby/1.8/ruby-#{RUBY_VERSION}.tar.gz")
     end
 
     # extract ruby
-    file "#{CROSS}/ruby-1.8.6-p287" => [CROSS, "#{STASH}/ruby-1.8.6-p287.tar.gz"] do
+    file "#{CROSS}/ruby-#{RUBY_VERSION}" => [CROSS, "#{STASH}/ruby-#{RUBY_VERSION}.tar.gz"] do
       when_writing "extracting ruby" do
         cd(CROSS) do
-          sh "tar zxvf #{STASH}/ruby-1.8.6-p287.tar.gz"
+          sh "tar zxvf #{STASH}/ruby-#{RUBY_VERSION}.tar.gz"
         end
       end
     end
 
     # compile ruby
-    file "#{CROSS}/bin/ruby.exe" => "#{CROSS}/ruby-1.8.6-p287/Makefile" do
+    file "#{CROSS}/bin/ruby.exe" => "#{CROSS}/ruby-#{RUBY_VERSION}/Makefile" do
       when_writing "cross-compiling ruby" do
-        cd "#{CROSS}/ruby-1.8.6-p287" do
+        cd "#{CROSS}/ruby-#{RUBY_VERSION}" do
           sh "make"
           sh "make install"
         end
       end
     end
 
-    file "#{CROSS}/ruby-1.8.6-p287/Makefile" => ["#{CROSS}/ruby-1.8.6-p287/Makefile.in.bak"] do
+    file "#{CROSS}/ruby-#{RUBY_VERSION}/Makefile" => ["#{CROSS}/ruby-#{RUBY_VERSION}/Makefile.in.bak"] do
       when_writing "Configuring ruby compilation" do
-        cd "#{CROSS}/ruby-1.8.6-p287" do
+        cd "#{CROSS}/ruby-#{RUBY_VERSION}" do
           buildopts = if File.exists?('/usr/bin/i586-mingw32msvc-gcc')
             '--host=i586-mingw32msvc --target=i386-mingw32 --build=i686-linux'
           else
@@ -83,16 +85,16 @@ namespace :build do
       end
     end
 
-    file "#{CROSS}/ruby-1.8.6-p287/Makefile.in.bak" => "#{CROSS}/ruby-1.8.6-p287" do
+    file "#{CROSS}/ruby-#{RUBY_VERSION}/Makefile.in.bak" => "#{CROSS}/ruby-#{RUBY_VERSION}" do
       when_writing "correcting Makefile.in" do
-        cd "#{CROSS}/ruby-1.8.6-p287" do
+        cd "#{CROSS}/ruby-#{RUBY_VERSION}" do
           cp "Makefile.in", "Makefile.in.bak"
           str = ''
           File.open("Makefile.in", 'rb') do |f|
             f.each_line do |line|
               if line =~ /^\s*ALT_SEPARATOR =/
                 warn "replacing alt sep!"
-                str << "\t\t " + 'ALT_SEPARATOR = "\\\\\"; \\'
+                str << "\t\t " + 'ALT_SEPARATOR = "\\\\"; \\'
                 str << "\n"
               else
                 str << line
@@ -109,7 +111,7 @@ namespace :build do
     # compilation fails without this.
     task :correct_makefile_in do
       when_writing "correcting Makefile.in" do
-        makefile_in = "#{CROSS}/ruby-1.8.6-p287/Makefile.in"
+        makefile_in = "#{CROSS}/ruby-#{RUBY_VERSION}/Makefile.in"
       end
     end
   end
