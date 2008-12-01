852f9971afd097d258c3f37ec54a1115f3f89a0c
Alex Coles <alexbcoles@mac.com>
2008-12-01 04:12:05 +0100

    Fix definitions of whether generated keys in JDBC API are supported
    * Workarounds will need to be developed for HSQLDB + Sqlite3.
    
    Signed-off-by: Alex Coles <alexbcoles@mac.com>

--- do_hsqldb/ext-java/src/main/java/do_hsqldb/HsqldbDriverDefinition.java
+++ do_hsqldb/ext-java/src/main/java/do_hsqldb/HsqldbDriverDefinition.java
@@ -6,7 +6,7 @@ public class HsqldbDriverDefinition extends AbstractDriverDefinition {
 
     public boolean supportsJdbcGeneratedKeys()
     {
-        return true;
+        return false;
     }
 
 }
