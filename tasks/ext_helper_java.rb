bd7b3df7cdca9ab231fa5711cfcca269f681c40a
Alex Coles <alex@alexcolesportfolio.com>
2009-01-05 01:10:27 +0100

    [do_jdbc] A few fixes to get building on JRuby working once again
    * Bracketed out rake-compiler requirement on JRuby.
    * Reinstate the "compile" alias for compile:jruby, if running on that
      platform.
    
    Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

--- tasks/ext_helper_java.rb
+++ tasks/ext_helper_java.rb
@@ -5,27 +5,27 @@ def setup_extension_java(extension_name, gem_spec = nil)
   ext_name = "#{extension_name}.jar"
   directory 'lib'
 
-  unless File.directory?("ext/#{extension_name}")
+  desc 'Compile Extension for current Ruby (= compile:jruby)'
+  task :compile => [ 'compile:jruby' ] if JRUBY
 
-    namespace :compile do
-      desc "Compile Java Extension for JRuby"
-      #task :jruby => ["lib/#{ext_name}"]
+  namespace :compile do
+    desc "Compile Java Extension for JRuby"
+    #task :jruby => ["lib/#{ext_name}"]
 
-      task :jruby do
-        begin
-          # gem 'buildr', '1.3.1.1'
-          # require 'buildr'
-          # FIXME: this is throwing rspec activation errors
-          sh %{jruby -S buildr package}
+    task :jruby do
+      begin
+        # gem 'buildr', '1.3.1.1'
+        # require 'buildr'
+        # FIXME: this is throwing rspec activation errors
+        sh %{jruby -S buildr package}
 
-          buildr_output = extension_name.gsub(/_(ext)$/, '-\1-java-1.0.jar')
-          cp "ext-java/target/#{buildr_output}", "lib/#{ext_name}"
-        rescue LoadError
-          puts "#{spec.name} requires the buildr gem to compile the Java extension"
-        end
+        buildr_output = extension_name.gsub(/_(ext)$/, '-\1-java-1.0.jar')
+        cp "ext-java/target/#{buildr_output}", "lib/#{ext_name}"
+      rescue LoadError
+        puts "#{spec.name} requires the buildr gem to compile the Java extension"
       end
     end
-    file "lib/#{ext_name}" => 'compile:jruby'
-
   end
+  file "lib/#{ext_name}" => 'compile:jruby'
+
 end
