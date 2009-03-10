8682ec9c5f8cf5373ebd8037ee960e936e84c4db
Alex Coles <alex@alexcolesportfolio.com>
2009-03-10 13:03:26 +0100

    [do_derby][do_hsqldb][do_h2] Convert to use shared spec suite
    Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

--- do_derby/spec/spec_helper.rb
+++ do_derby/spec/spec_helper.rb
@@ -6,6 +6,7 @@ gem 'rspec', '~>1.1.12'
 require 'spec'
 
 require 'date'
+require 'ostruct'
 require 'pathname'
 require 'fileutils'
 
@@ -27,7 +28,7 @@ require 'do_derby'
 log_path = File.expand_path(File.join(File.dirname(__FILE__), '..', 'log', 'do.log'))
 FileUtils.mkdir_p(File.dirname(log_path))
 
-DataObjects::Derby.logger = DataObjects::Logger.new(log_path, 0)
+DataObjects::Derby.logger = DataObjects::Logger.new(log_path, :debug)
 
 at_exit { DataObjects.logger.flush }
 
@@ -35,56 +36,44 @@ Spec::Runner.configure do |config|
   config.include(DataObjects::Spec::PendingHelpers)
 end
 
-module DerbySpecHelpers
+CONFIG = OpenStruct.new
+# CONFIG.scheme   = 'derby'
+# CONFIG.user     = ENV['DO_DERBY_USER'] || 'derby'
+# CONFIG.pass     = ENV['DO_DERBY_PASS'] || ''
+# CONFIG.host     = ENV['DO_DERBY_HOST'] || ''
+# CONFIG.port     = ENV['DO_DERBY_PORT'] || ''
+# CONFIG.database = ENV['DO_DERBY_DATABASE'] || "#{File.expand_path(File.dirname(__FILE__))}/testdb"
 
-  def insert(query, *args)
-    result = @connection.create_command(query).execute_non_query(*args)
-    result.insert_id
-  end
+CONFIG.uri = ENV["DO_DERBY_SPEC_URI"] || "jdbc:derby:testdb;create=true"
 
-  def exec(query, *args)
-    @connection.create_command(query).execute_non_query(*args)
-  end
-
-  def select(query, types = nil, *args)
-    begin
-      command = @connection.create_command(query)
-      command.set_types types unless types.nil?
-      reader = command.execute_reader(*args)
-      reader.next!
-      yield reader if block_given?
-    ensure
-      reader.close if reader
-    end
-  end
-
+module DataObjectsSpecHelpers
 
   def setup_test_environment
-    @connection = DataObjects::Connection.new("jdbc:derby:testdb;create=true")
+    conn = DataObjects::Connection.new(CONFIG.uri)
 
     # Derby does not support DROP TABLE IF EXISTS
     begin
-        @connection.create_command(<<-EOF).execute_non_query
+        conn.create_command(<<-EOF).execute_non_query
           DROP TABLE invoices
         EOF
     rescue DerbyError
     end
 
     begin
-        @connection.create_command(<<-EOF).execute_non_query
+        conn.create_command(<<-EOF).execute_non_query
             DROP TABLE users
         EOF
     rescue DerbyError
     end
 
     begin
-        @connection.create_command(<<-EOF).execute_non_query
+        conn.create_command(<<-EOF).execute_non_query
           DROP TABLE widgets
         EOF
     rescue DerbyError
     end
 
-    @connection.create_command(<<-EOF).execute_non_query
+    conn.create_command(<<-EOF).execute_non_query
       CREATE TABLE users (
         id                INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         name              VARCHAR(200) default 'Billy',
@@ -92,14 +81,14 @@ module DerbySpecHelpers
       )
     EOF
 
-    @connection.create_command(<<-EOF).execute_non_query
+    conn.create_command(<<-EOF).execute_non_query
       CREATE TABLE invoices (
         id                INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         invoice_number    VARCHAR(50) NOT NULL
       )
     EOF
 
-    @connection.create_command(<<-EOF).execute_non_query
+    conn.create_command(<<-EOF).execute_non_query
       CREATE TABLE widgets (
         id                INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         code              CHAR(8) DEFAULT 'A14',
@@ -114,23 +103,21 @@ module DerbySpecHelpers
         flags             SMALLINT DEFAULT 0,
         number_in_stock   SMALLINT DEFAULT 500,
         number_sold       INTEGER DEFAULT 0,
-        super_number      BIGINT DEFAULT 9223372036854775807
+        super_number      BIGINT DEFAULT 9223372036854775807,
+        weight            REAL DEFAULT 10.23,
+        cost1             DOUBLE DEFAULT 10.23,
+        cost2             DECIMAL(8,2) DEFAULT 50.23,
+        release_date      DATE DEFAULT '2008-02-14',
+        release_datetime  TIMESTAMP DEFAULT '2008-02-14 00:31:12',
+        release_timestamp TIMESTAMP DEFAULT '2008-02-14 00:31:31'
       )
     EOF
 
-    # REMOVED:
-    # weight            FLOAT DEFAULT 1.23,
-    # cost1             REAL DEFAULT 10.23,
-    # cost2             DECIMAL DEFAULT 50.23,
-    # release_date      DATE DEFAULT '2008-02-14',
-    # release_datetime  DATETIME, DEFAULT '2008-02-14 00:31:12',
-    # release_timestamp TIMESTAMP, DEFAULT '2008-02-14 00:31:31'
-    #
-    # XXX: HSQLDB has no ENUM
+    # XXX: Derby has no ENUM
     # status` enum('active','out of stock') NOT NULL default 'active'
 
     1.upto(16) do |n|
-      @connection.create_command(<<-EOF).execute_non_query
+      conn.create_command(<<-EOF).execute_non_query
         INSERT INTO widgets(
           code,
           name,
@@ -138,7 +125,8 @@ module DerbySpecHelpers
           description,
           ad_description,
           whitepaper_text,
-          super_number)
+          super_number,
+          weight)
         VALUES (
           'W#{n.to_s.rjust(7,"0")}',
           'Widget #{n}',
@@ -146,7 +134,8 @@ module DerbySpecHelpers
           'This is a description',
           'Buy this product now!',
           'Utilizing blah blah blah',
-          1234)
+          1234,
+          13.4)
       EOF
 
       # Removed
@@ -154,6 +143,16 @@ module DerbySpecHelpers
       #           ad_image,
       #           cad_drawing,
       # XXX: figure out how to insert BLOBS
+
+      conn.create_command(<<-EOF).execute_non_query
+        update widgets set flags = 1 where id = 2
+      EOF
+
+      conn.create_command(<<-EOF).execute_non_query
+        update widgets set ad_description = NULL where id = 3
+      EOF
+
+      conn.close
     end
 
   end
--- do_hsqldb/spec/spec_helper.rb
+++ do_hsqldb/spec/spec_helper.rb
@@ -6,6 +6,7 @@ gem 'rspec', '~>1.1.12'
 require 'spec'
 
 require 'date'
+require 'ostruct'
 require 'pathname'
 require 'fileutils'
 
@@ -27,7 +28,7 @@ require 'do_hsqldb'
 log_path = File.expand_path(File.join(File.dirname(__FILE__), '..', 'log', 'do.log'))
 FileUtils.mkdir_p(File.dirname(log_path))
 
-DataObjects::Hsqldb.logger = DataObjects::Logger.new(log_path, 0)
+DataObjects::Hsqldb.logger = DataObjects::Logger.new(log_path, :debug)
 
 at_exit { DataObjects.logger.flush }
 
@@ -35,50 +36,34 @@ Spec::Runner.configure do |config|
   config.include(DataObjects::Spec::PendingHelpers)
 end
 
-module JdbcSpecHelpers
+CONFIG = OpenStruct.new
+# CONFIG.scheme   = 'hsqldb'
+# CONFIG.user     = ENV['DO_HSQLDB_USER'] || 'hsqldb'
+# CONFIG.pass     = ENV['DO_HSQLDB_PASS'] || ''
+# CONFIG.host     = ENV['DO_HSQLDB_HOST'] || ''
+# CONFIG.port     = ENV['DO_HSQLDB_PORT'] || ''
+# CONFIG.database = ENV['DO_HSQLDB_DATABASE'] || "#{File.expand_path(File.dirname(__FILE__))}/testdb"
 
-  # Copied wholesale from sqlite3 spec_helper.rb
-  def insert(query, *args)
-    result = @connection.create_command(query).execute_non_query(*args)
-    result.insert_id
-  end
+CONFIG.uri = ENV["DO_HSQLDB_SPEC_URI"] || "jdbc:hsqldb:mem"
 
-  def exec(query, *args)
-    @connection.create_command(query).execute_non_query(*args)
-  end
-
-  def select(query, types = nil, *args)
-    begin
-      command = @connection.create_command(query)
-      command.set_types types unless types.nil?
-      reader = command.execute_reader(*args)
-      reader.next!
-      yield reader if block_given?
-    ensure
-      reader.close if reader
-    end
-  end
-
-  def teardown_test_environment
-    @connection.close
-  end
+module DataObjectsSpecHelpers
 
   def setup_test_environment
-    @connection = DataObjects::Connection.new("jdbc:hsqldb:mem")
+    conn = DataObjects::Connection.new(CONFIG.uri)
 
-    @connection.create_command(<<-EOF).execute_non_query
+    conn.create_command(<<-EOF).execute_non_query
       DROP TABLE IF EXISTS invoices
     EOF
 
-    @connection.create_command(<<-EOF).execute_non_query
+    conn.create_command(<<-EOF).execute_non_query
       DROP TABLE IF EXISTS users
     EOF
 
-    @connection.create_command(<<-EOF).execute_non_query
+    conn.create_command(<<-EOF).execute_non_query
       DROP TABLE IF EXISTS widgets
     EOF
 
-    @connection.create_command(<<-EOF).execute_non_query
+    conn.create_command(<<-EOF).execute_non_query
       CREATE TABLE users (
         id                INTEGER IDENTITY,
         name              VARCHAR(200) default 'Billy' NULL,
@@ -86,14 +71,14 @@ module JdbcSpecHelpers
       )
     EOF
 
-    @connection.create_command(<<-EOF).execute_non_query
+    conn.create_command(<<-EOF).execute_non_query
       CREATE TABLE invoices (
         id                INTEGER IDENTITY,
         invoice_number    VARCHAR(50) NOT NULL
       )
     EOF
 
-    @connection.create_command(<<-EOF).execute_non_query
+    conn.create_command(<<-EOF).execute_non_query
       CREATE TABLE widgets (
         id                INTEGER IDENTITY,
         code              CHAR(8) DEFAULT 'A14' NULL,
@@ -121,7 +106,7 @@ module JdbcSpecHelpers
     # status` enum('active','out of stock') NOT NULL default 'active'
 
     1.upto(16) do |n|
-      @connection.create_command(<<-EOF).execute_non_query
+      conn.create_command(<<-EOF).execute_non_query
         INSERT INTO widgets(
           code,
           name,
@@ -132,7 +117,8 @@ module JdbcSpecHelpers
           ad_image,
           whitepaper_text,
           cad_drawing,
-          super_number)
+          super_number,
+          weight)
         VALUES (
           'W#{n.to_s.rjust(7,"0")}',
           'Widget #{n}',
@@ -143,10 +129,20 @@ module JdbcSpecHelpers
           '4f3d4331434343434331',
           'Utilizing blah blah blah',
           '4f3d4331434343434331',
-          1234);
+          1234,
+          13.4);
+      EOF
+
+      conn.create_command(<<-EOF).execute_non_query
+        update widgets set flags = 1 where id = 2
+      EOF
+
+      conn.create_command(<<-EOF).execute_non_query
+        update widgets set ad_description = NULL where id = 3
       EOF
 
       ## TODO: change the hexadecimal examples
+      conn.close
     end
 
   end
