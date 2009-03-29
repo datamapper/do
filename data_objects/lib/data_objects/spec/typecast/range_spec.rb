8a7b6ed99d215d4341762fd0e0817f86936ac4ea
Alex Coles <alex@alexcolesportfolio.com>
2009-03-29 22:59:20 +0200

    Split Array / Range tests into separate .rb files
    Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

--- data_objects/lib/data_objects/spec/typecast/range_spec.rb
+++ data_objects/lib/data_objects/spec/typecast/range_spec.rb
@@ -1,4 +1,4 @@
-share_examples_for 'a driver supporting Array and Range' do
+share_examples_for 'supporting Range' do
 
   include DataObjectsSpecHelpers
 
@@ -12,26 +12,6 @@ share_examples_for 'a driver supporting Array and Range' do
 
   after :each do
     @connection.close
-  end
-
-  describe 'passing an Array as a parameter in execute_reader' do
-
-      before  do
-        @reader = @connection.create_command("SELECT * FROM widgets WHERE id in ?").execute_reader([2,3,4,5])
-      end
-
-      after do
-        @reader.close
-      end
-
-      it 'should return correct number of rows' do
-        counter  = 0
-        while(@reader.next!) do
-          counter += 1
-        end
-        counter.should == 4
-      end
-
   end
 
   describe 'passing a Range as a parameter in execute_reader' do
@@ -51,6 +31,6 @@ share_examples_for 'a driver supporting Array and Range' do
         end
         counter.should == 4
       end
-      
+
   end
 end
--- do_mysql/spec/typecast/array_range_spec.rb
+++
@@ -1,8 +0,0 @@
-# encoding: utf-8
-
-require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
-require 'data_objects/spec/typecast/array_range_spec'
-
-describe 'DataObjects::Mysql with Array and Range' do
-  it_should_behave_like 'a driver supporting Array and Range'
-end
