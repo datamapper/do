3fc9fab76294babd85bf34df80c06773589a1816
Piotr Gega (pietia) <pietia.moo@gmail.com>
2009-03-29 22:22:59 +0800

    Add simple spec to test code against Array and Range as a parameters of Command#execute_reader when we use SQL queries with ... between ?' or '... in ?'
    Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

---
+++ do_derby/spec/typecast/array_range_spec.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
+require 'data_objects/spec/typecast/array_range_spec'
+
+describe 'DataObjects::Derby with Array and Range' do
+  it_should_behave_like 'a driver supporting Array and Range'
+end
