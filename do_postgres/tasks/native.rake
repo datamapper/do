datamapper/do

commit 0f8c3f9574fd89ff742268d8da2fb8d925ad7463

Get rid of Hoe and rework building in preparation for easy multiple binary gem release

do_postgres/tasks/native.rake
@@ -2,7 +2,7 @@ begin
   gem('rake-compiler')
   require 'rake/extensiontask'
 
-  Rake::ExtensionTask.new('do_postgres_ext', HOE.spec) do |ext|
+  Rake::ExtensionTask.new('do_postgres_ext', GEM_SPEC) do |ext|
   
     postgres_lib = File.expand_path(File.join(File.dirname(__FILE__), '..', 'vendor', 'pgsql'))
 
@@ -24,5 +24,5 @@ begin
   end
 rescue LoadError
   warn "To cross-compile, install rake-compiler (gem install rake-compiler)"
-  setup_c_extension('do_postgres_ext', HOE.spec)
+  setup_c_extension('do_postgres_ext', GEM_SPEC)
 end

do_sqlite3/tasks/native.rake
@@ -8,7 +8,7 @@ begin
     Rake::Task['compile:jruby'].invoke
   end
 
-  Rake::ExtensionTask.new('do_sqlite3_ext', HOE.spec) do |ext|
+  Rake::ExtensionTask.new('do_sqlite3_ext', GEM_SPEC) do |ext|
 
     sqlite3_lib = File.expand_path(File.join(File.dirname(__FILE__), '..', 'vendor', 'sqlite3'))
 
@@ -24,5 +24,5 @@ begin
   end
 rescue LoadError
   warn "To cross-compile, install rake-compiler (gem install rake-compiler)"
-  setup_c_extension('do_sqlite3_ext', HOE.spec)
+  setup_c_extension('do_sqlite3_ext', GEM_SPEC)
 end

do_mysql/tasks/native.rake
@@ -1,8 +1,8 @@
 begin
   gem('rake-compiler')
   require 'rake/extensiontask'
-  
-  Rake::ExtensionTask.new('do_mysql_ext', HOE.spec) do |ext|
+
+  Rake::ExtensionTask.new('do_mysql_ext', GEM_SPEC) do |ext|
 
     mysql_lib = File.expand_path(File.join(File.dirname(__FILE__), '..', 'vendor', "mysql-#{BINARY_VERSION}-win32"))
   
@@ -20,5 +20,12 @@ begin
   end
 rescue LoadError
   warn "To cross-compile, install rake-compiler (gem install rake-compiler)"
-  setup_c_extension('do_mysql_ext', HOE.spec)
+
+  if (tasks_dir = ROOT.parent + 'tasks').directory?
+    require tasks_dir + 'ext_helper'
+    require tasks_dir + 'ext_helper_java'
+
+    setup_c_extension("#{GEM_SPEC.name}_ext", GEM_SPEC)
+    setup_java_extension("#{GEM_SPEC.name}_ext", GEM_SPEC)
+  end
 end

data_objects/Rakefile
@@ -1,6 +1,8 @@
-require 'pathname'
 require 'rubygems'
-require 'spec/rake/spectask'
+require 'rake'
+require 'rake/clean'
+
+require 'pathname'
 require 'lib/data_objects/version'
 
 ROOT    = Pathname(__FILE__).dirname.expand_path
@@ -8,52 +10,7 @@ JRUBY   = RUBY_PLATFORM =~ /java/
 WINDOWS = Gem.win_platform?
 SUDO    = (WINDOWS || JRUBY) ? '' : ('sudo' unless ENV['SUDOLESS'])
 
-AUTHOR = "Dirkjan Bussink"
-EMAIL  = "d.bussink@gmail.com"
-GEM_NAME = "data_objects"
-GEM_VERSION = DataObjects::VERSION
-GEM_DEPENDENCIES = ["addressable", "~>2.0"], ["extlib", "~>0.9.11"]
-GEM_CLEAN = "{coverage,doc,log}/", "profile_results.*", "**/.*.sw?", "*.gem", ".config", "**/.DS_Store"
-GEM_EXTRAS = {}
+Dir['tasks/*.rake'].each { |f| import f }
 
-PROJECT_NAME = "dorb"
-PROJECT_URL  = "http://rubyforge.org/projects/dorb"
-PROJECT_DESCRIPTION = PROJECT_SUMMARY = "The Core DataObjects class"
+CLOBBER.include("**/*.rbc")
 
-if (tasks_dir = ROOT.parent + 'tasks').directory?
-  require tasks_dir + 'hoe'
-end
-
-def sudo_gem(cmd)
-  sh "#{SUDO} #{RUBY} -S gem #{cmd}", :verbose => false
-end
-
-# Installation
-
-desc "Install #{GEM_NAME} #{GEM_VERSION}"
-task :install => [ :package ] do
-  sudo_gem "install --local pkg/#{GEM_NAME}-#{GEM_VERSION} --no-update-sources"
-end
-
-desc "Uninstall #{GEM_NAME} #{GEM_VERSION}"
-task :uninstall => [ :clean ] do
-  sudo_gem "uninstall #{GEM_NAME} -v#{GEM_VERSION} -I -x"
-end
-
-# Specs
-
-desc 'Run specifications'
-Spec::Rake::SpecTask.new(:spec) do |t|
-  t.spec_opts << '--options' << ROOT + 'spec/spec.opts'
-  t.spec_files = Pathname.glob(ENV['FILES'] || 'spec/**/*_spec.rb').map { |f| f.to_s }
-
-  begin
-    # RCov is run by default, except on the JRuby platform
-    t.rcov = JRUBY ? false : (ENV.has_key?('NO_RCOV') ? ENV['NO_RCOV'] != 'true' : true)
-    t.rcov_opts << '--exclude' << 'spec'
-    t.rcov_opts << '--text-summary'
-    t.rcov_opts << '--sort' << 'coverage' << '--sort-reverse'
-  rescue Exception
-    # rcov not installed
-  end
-end

data_objects/data_objects.gemspec
@@ -1,0 +1,131 @@
+--- !ruby/object:Gem::Specification 
+name: data_objects
+version: !ruby/object:Gem::Version 
+  version: 0.9.12
+platform: ruby
+authors: 
+- Dirkjan Bussink
+autorequire: 
+bindir: bin
+cert_chain: []
+
+date: 2009-05-05 00:00:00 +02:00
+default_executable: 
+dependencies: 
+- !ruby/object:Gem::Dependency 
+  name: addressable
+  type: :runtime
+  version_requirement: 
+  version_requirements: !ruby/object:Gem::Requirement 
+    requirements: 
+    - - ~>
+      - !ruby/object:Gem::Version 
+        version: 2.0.0
+    version: 
+- !ruby/object:Gem::Dependency 
+  name: extlib
+  type: :runtime
+  version_requirement: 
+  version_requirements: !ruby/object:Gem::Requirement 
+    requirements: 
+    - - ~>
+      - !ruby/object:Gem::Version 
+        version: 0.9.12
+    version: 
+- !ruby/object:Gem::Dependency 
+  name: rspec
+  type: :development
+  version_requirement: 
+  version_requirements: !ruby/object:Gem::Requirement 
+    requirements: 
+    - - ~>
+      - !ruby/object:Gem::Version 
+        version: 1.2.0
+    version: 
+description: Provide a standard and simplified API for communicating with RDBMS from Ruby
+email: d.bussink@gmail.com
+executables: []
+
+extensions: []
+
+extra_rdoc_files: []
+
+files: 
+- lib/data_objects/command.rb
+- lib/data_objects/connection.rb
+- lib/data_objects/logger.rb
+- lib/data_objects/quoting.rb
+- lib/data_objects/reader.rb
+- lib/data_objects/result.rb
+- lib/data_objects/spec/command_spec.rb
+- lib/data_objects/spec/connection_spec.rb
+- lib/data_objects/spec/encoding_spec.rb
+- lib/data_objects/spec/quoting_spec.rb
+- lib/data_objects/spec/reader_spec.rb
+- lib/data_objects/spec/result_spec.rb
+- lib/data_objects/spec/typecast/array_spec.rb
+- lib/data_objects/spec/typecast/bigdecimal_spec.rb
+- lib/data_objects/spec/typecast/boolean_spec.rb
+- lib/data_objects/spec/typecast/byte_array_spec.rb
+- lib/data_objects/spec/typecast/class_spec.rb
+- lib/data_objects/spec/typecast/date_spec.rb
+- lib/data_objects/spec/typecast/datetime_spec.rb
+- lib/data_objects/spec/typecast/float_spec.rb
+- lib/data_objects/spec/typecast/integer_spec.rb
+- lib/data_objects/spec/typecast/ipaddr_spec.rb
+- lib/data_objects/spec/typecast/nil_spec.rb
+- lib/data_objects/spec/typecast/range_spec.rb
+- lib/data_objects/spec/typecast/string_spec.rb
+- lib/data_objects/spec/typecast/time_spec.rb
+- lib/data_objects/transaction.rb
+- lib/data_objects/uri.rb
+- lib/data_objects/version.rb
+- lib/data_objects.rb
+- spec/command_spec.rb
+- spec/connection_spec.rb
+- spec/do_mock.rb
+- spec/lib/pending_helpers.rb
+- spec/lib/rspec_immediate_feedback_formatter.rb
+- spec/reader_spec.rb
+- spec/result_spec.rb
+- spec/spec_helper.rb
+- spec/transaction_spec.rb
+- spec/uri_spec.rb
+- tasks/gem.rake
+- tasks/install.rake
+- tasks/release.rake
+- tasks/spec.rake
+- LICENSE
+- Rakefile
+- History.txt
+- README.txt
+has_rdoc: false
+homepage: http://github.com/datamapper/do
+licenses: 
+- MIT
+post_install_message: 
+rdoc_options: []
+
+require_paths: 
+- lib
+required_ruby_version: !ruby/object:Gem::Requirement 
+  requirements: 
+  - - ">="
+    - !ruby/object:Gem::Version 
+      version: "0"
+  version: 
+required_rubygems_version: !ruby/object:Gem::Requirement 
+  requirements: 
+  - - ">="
+    - !ruby/object:Gem::Version 
+      version: "0"
+  version: 
+requirements: []
+
+rubyforge_project: extlib
+rubygems_version: 1.3.2
+signing_key: 
+specification_version: 3
+summary: DataObjects basic API and shared driver specifications
+test_files: []
+

data_objects/tasks/gem.rake
@@ -1,0 +1,50 @@
+require 'rubygems/package_task'
+
+GEM_SPEC = Gem::Specification.new do |s|
+  # basic information
+  s.name        = "data_objects"
+  s.version     = DataObjects::VERSION
+  s.platform    = Gem::Platform::RUBY
+
+  # description and details
+  s.summary     = 'DataObjects basic API and shared driver specifications'
+  s.description = "Provide a standard and simplified API for communicating with RDBMS from Ruby"
+
+  # dependencies
+  s.add_dependency "addressable", "~>2.0.0"
+  s.add_dependency "extlib", "~>0.9.12"
+
+  # development dependencies
+  s.add_development_dependency 'rspec', '~>1.2.0'
+
+  # components, files and paths
+  s.files = FileList["lib/**/*.rb", "spec/**/*.rb", "tasks/**/*.rake",
+                      "LICENSE", "Rakefile", "*.{rdoc,txt,yml}"]
+
+  s.require_path = 'lib'
+
+  # documentation
+  s.has_rdoc = false
+
+  # project information
+  s.homepage          = 'http://github.com/datamapper/do'
+  s.rubyforge_project = 'dorb'
+  s.licenses          = ['MIT']
+
+  # author and contributors
+  s.author      = 'Dirkjan Bussink'
+  s.email       = 'd.bussink@gmail.com'
+end
+
+gem_package = Gem::PackageTask.new(GEM_SPEC) do |pkg|
+  pkg.need_tar = false
+  pkg.need_zip = false
+end
+
+file "#{GEM_SPEC.name}.gemspec" => ['Rakefile', 'tasks/gem.rake'] do |t|
+  puts "Generating #{t.name}"
+  File.open(t.name, 'w') { |f| f.puts GEM_SPEC.to_yaml }
+end
+
+desc "Generate or update the standalone gemspec file for the project"
+task :gemspec => ["#{GEM_SPEC.name}.gemspec"]

data_objects/tasks/install.rake
@@ -2,15 +2,12 @@ def sudo_gem(cmd)
   sh "#{SUDO} #{RUBY} -S gem #{cmd}", :verbose => false
 end
 
-# Installation
-
 desc "Install #{GEM_SPEC.name} #{GEM_SPEC.version}"
 task :install => [ :package ] do
-  sudo_gem "install pkg/#{GEM_SPEC.name}-#{GEM_SPEC.version} --no-update-sources"
+  sudo_gem "install --local pkg/#{GEM_SPEC.name}-#{GEM_SPEC.version} --no-update-sources"
 end
 
 desc "Uninstall #{GEM_SPEC.name} #{GEM_SPEC.version}"
 task :uninstall => [ :clean ] do
   sudo_gem "uninstall #{GEM_SPEC.name} -v#{GEM_SPEC.version} -I -x"
 end
-

data_objects/tasks/release.rake
@@ -1,0 +1,74 @@
+begin
+  gem 'rubyforge', '~> 1.0.1'
+  require 'rubyforge'
+rescue Exception
+  nil
+end
+
+if defined?(RubyForge) then
+  if defined?(GEM_SPEC) then
+    desc 'Package and upload to RubyForge'
+    task :release => [:clobber, :package] do |t|
+      ver = ENV['VERSION'] or fail "Must supply VERSION (rake release VERSION=x.y.z)."
+
+      # compare versions to avoid mistakes
+      unless ver == GEM_SPEC.version.to_s then
+        fail "Version mismatch (supplied and specification versions differ)."
+      end
+
+      # no rubyforge project? no release for you!
+      if GEM_SPEC.rubyforge_project == 'TODO' or GEM_SPEC.rubyforge_project.nil? then
+        fail "Must define rubyforge_project in your gem specification."
+      end
+
+      # instantiate a RubyForge object
+      rf = RubyForge.new
+
+      # read project info and overview
+      notes = begin
+                r = File.read("README.rdoc")
+                r.split(/^(=+ .*)/)[1..4].join.strip
+              rescue
+                warn "Missing README.rdoc"
+                ''
+              end
+
+      # read changes
+      changes = begin
+                  h = File.read("History.txt")
+                  h.split(/^(==+ .*)/)[1..2].join.strip
+                rescue
+                  warn "Missing History.txt"
+                  ''
+                end
+
+      # build the configuration for the release
+      config = Hash.new
+      config["release_notes"] = notes
+      config["release_changes"] = changes
+      config["preformatted"] = true
+
+      # prepare configuration
+      rf.configure config
+
+      files = FileList["pkg/#{GEM_SPEC.name}-#{GEM_SPEC.version}*.*"].to_a
+      fail "No files found for the release." if files.empty?
+
+      puts "Logging in RubyForge..."
+      rf.login
+
+      puts "Files to upload:"
+      files.each do |f|
+        puts "  * #{f}"
+      end
+
+      puts "Releasing #{GEM_SPEC.name} version #{GEM_SPEC.version}..."
+      rf.add_release GEM_SPEC.rubyforge_project, GEM_SPEC.name, GEM_SPEC.version, *files
+      puts "Done."
+    end
+  else
+    warn "no GEM_SPEC is found or defined. 'release' task cannot work without it."
+  end
+else
+  warn "rubyforge gem is required to generate releases, please install it (gem install rubyforge)."
+end

commit dc43a7070034f4b736d14b6d85a178326608d111

[do_postgres][do_sqlite3] Fix compilation when not using rake-compiler
* Should also fix compilation under JRuby, broken after
  changes introduced in 0f8c3f9.

Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

do_postgres/tasks/native.rake
@@ -24,5 +24,12 @@ begin
   end
 rescue LoadError
   warn "To cross-compile, install rake-compiler (gem install rake-compiler)"
-  setup_c_extension('do_postgres_ext', GEM_SPEC)
+
+  if (tasks_dir = ROOT.parent + 'tasks').directory?
+    require tasks_dir + 'ext_helper'
+    require tasks_dir + 'ext_helper_java'
+
+    setup_c_extension("#{GEM_SPEC.name}_ext", GEM_SPEC)
+    setup_java_extension("#{GEM_SPEC.name}_ext", GEM_SPEC)
+  end
 end

do_sqlite3/tasks/native.rake
@@ -24,5 +24,12 @@ begin
   end
 rescue LoadError
   warn "To cross-compile, install rake-compiler (gem install rake-compiler)"
-  setup_c_extension('do_sqlite3_ext', GEM_SPEC)
+
+  if (tasks_dir = ROOT.parent + 'tasks').directory?
+    require tasks_dir + 'ext_helper'
+    require tasks_dir + 'ext_helper_java'
+
+    setup_c_extension("#{GEM_SPEC.name}_ext", GEM_SPEC)
+    setup_java_extension("#{GEM_SPEC.name}_ext", GEM_SPEC)
+  end
 end