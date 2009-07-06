d23218d4bf46d87a0c03ac199f40e7640a5c706d
Dirkjan Bussink <d.bussink@gmail.com>
2009-07-06 22:40:31 +0200

    Explain why auto casting isn't supported for certain types

--- do_sqlite3/spec/typecast/datetime_spec.rb
+++ do_sqlite3/spec/typecast/datetime_spec.rb
@@ -3,10 +3,9 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
 require 'data_objects/spec/typecast/datetime_spec'
 
-describe 'DataObjects::Sqlite3 with DateTime' do
-  it_should_behave_like 'supporting DateTime'
-end
+# Sqlite3 doesn't support datetimes natively, so autocasting is not available:
+# http://www.sqlite.org/datatype3.html
 
 describe 'DataObjects::Sqlite3 with DateTime' do
-#  it_should_behave_like 'supporting DateTime autocasting'
+  it_should_behave_like 'supporting DateTime'
 end
