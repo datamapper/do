f447b173123aae86067bf4cd77a39dd74f55d13b
Piotr Gega (pietia) <pietia.moo@gmail.com>
2009-07-06 16:59:24 +0200

    Added Postgres specyfic typecasting for bytea

--- do_postgres/ext-java/src/main/java/do_postgres/PostgresDriverDefinition.java
+++ do_postgres/ext-java/src/main/java/do_postgres/PostgresDriverDefinition.java
@@ -1,7 +1,15 @@
 package do_postgres;
 
 import data_objects.drivers.AbstractDriverDefinition;
+import data_objects.RubyType;
+
 import java.util.Properties;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Types;
+
+import org.jruby.runtime.builtin.IRubyObject;
+import org.jruby.RubyString;
 
 public class PostgresDriverDefinition extends AbstractDriverDefinition {
 
@@ -28,6 +36,27 @@ public class PostgresDriverDefinition extends AbstractDriverDefinition {
     public boolean supportsConnectionEncodings()
     {
         return true;
+    }
+
+    @Override	
+    public void setPreparedStatementParam(PreparedStatement ps,
+            IRubyObject arg, int idx) throws SQLException {
+        switch (RubyType.getRubyType(arg.getType().getName())) {
+        case BYTE_ARRAY:
+            final int jdbcType = ps.getParameterMetaData().getParameterType(idx);
+            switch(jdbcType){
+            case Types.BINARY:
+                ps.setBytes(idx, ((RubyString) arg).getBytes());
+                break;
+            default:
+                //XXX Postgres doesn't typecast bytea type to integer,decimal etc
+                //so it's done manually by the driver
+                ps.setObject(idx,arg.asJavaString(),jdbcType);
+            }
+            break;
+        default:
+            super.setPreparedStatementParam(ps, arg, idx);
+        }
     }
 
     @Override
