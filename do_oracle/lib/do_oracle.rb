a12ecca00a2d52596a912d335e39d6b325e1f726
Raimonds Simanovskis <raimonds.simanovskis@gmail.com>
2009-07-16 22:52:20 +0300

    [do_oracle] do not load do_oracle/transaction.rb for JDBC driver (transactions implemented in Java side)

--- do_oracle/lib/do_oracle.rb
+++ do_oracle/lib/do_oracle.rb
@@ -22,7 +22,10 @@ end
 
 require 'do_oracle_ext'
 require File.expand_path(File.join(File.dirname(__FILE__), 'do_oracle', 'version'))
-require File.expand_path(File.join(File.dirname(__FILE__), 'do_oracle', 'transaction'))
+if RUBY_PLATFORM !~ /java/
+  # JDBC driver has transactions implementation in Java
+  require File.expand_path(File.join(File.dirname(__FILE__), 'do_oracle', 'transaction'))
+end
 
 if RUBY_PLATFORM !~ /java/
   module DataObjects
