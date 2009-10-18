1b6634f16a2ba6698b2740b46760d1695c413f90
Alex Coles <alex@alexcolesportfolio.com>
2009-10-18 15:04:59 +0200

    [jdbc_drivers] Bump bundled sqlserver gem(jTDS) to 1.2.4
    Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

--- jdbc_drivers/sqlserver/lib/do_jdbc/sqlserver_version.rb
+++ jdbc_drivers/sqlserver/lib/do_jdbc/sqlserver_version.rb
@@ -1,7 +1,7 @@
 module DataObjects
   module Jdbc
     module SqlServer
-      VERSION  = '1.2.2'
+      VERSION  = '1.2.4'
       JAR_NAME = "jtds-#{DataObjects::Jdbc::SqlServer::VERSION}.jar"
     end
   end
