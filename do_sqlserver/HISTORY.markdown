609e0b23ceee07b5a0bc1833530d96b85f532625
Alex Coles <alex@alexcolesportfolio.com>
2009-10-18 15:31:24 +0200

    [do_sqlserver] Do not support ByteArray for time-being
    * Add comment as to why its not supported and link to the relevant
      jTDS bug (#getParameterMetadata is not returning the information
      we need to do a manual typecast).
    
    Signed-off-by: Alex Coles <alex@alexcolesportfolio.com>

--- do_sqlserver/HISTORY.markdown
+++ do_sqlserver/HISTORY.markdown
@@ -4,6 +4,8 @@ Initial release as part of mainline DO project.
 
 * Improvements
   * JRuby Support (using *do_jdbc*)
+* Known Issues:
+  * Writing Extlib::ByteArray is not currently supported.
 
 ## 0.0.1 2009-05-11
 
--- do_sqlserver/spec/typecast/byte_array_spec.rb
+++ do_sqlserver/spec/typecast/byte_array_spec.rb
@@ -4,5 +4,28 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
 require 'data_objects/spec/typecast/byte_array_spec'
 
 describe 'DataObjects::SqlServer with ByteArray' do
-  it_should_behave_like 'supporting ByteArray'
+  # it_should_behave_like 'supporting ByteArray'
+  #
+  # ByteArray is not yet supported on JRuby:
+  #
+  # Like Postgres, SQL Server doesn't typecast bytea type to integer, decimal,
+  # etc. In other words,
+  #   @connection.create_command("SELECT id FROM widgets WHERE id = ?").execute_reader(::Extlib::ByteArray.new("2"))
+  # results in the equivalent to the following query being executed:
+  #   SELECT id FROM widgets WHERE id = 0x32
+  # BUT 0x32 as a parameter = (decimal) 50
+  # NOT the ASCII char for '2'.
+  #
+  # Other drivers (Postgres) override #setPreparedStatementParam in their
+  # DriverDefinition implementations and use ps.getParameterMetadata() to let
+  # the JDBC driver handle the casting.
+  #
+  # Unfortunately, we can't rely on ps.getParameterMetadata() because of the
+  # following bug in jTDS:
+  # https://sourceforge.net/tracker/?func=detail&aid=2220192&group_id=33291&atid=407762
+  #  getParameterClassName(idx) => java.lang.Object
+  #  getParameterTypeName(idx)  => null
+  #  getParameterType(idx)      => 0 (NULL)
+  #
+  # Without this information we don't know what we should be casting to!
 end
