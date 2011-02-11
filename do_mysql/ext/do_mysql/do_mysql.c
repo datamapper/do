datamapper/do

commit 83d72d60209b34ede8eb2e3fd39758ccb43f6336

Consider DST status of the given date, not today.
In passing, use the thread-safe time.h functions, and don't assume
tm_isdst will always be 0 or 1.

Signed-off-by: Dirkjan Bussink <d.bussink@gmail.com>

do_mysql/ext/do_mysql/do_mysql.c
@@ -236,10 +236,10 @@ static VALUE parse_date_time(const char *date) {
 
 
   time_t gmt_offset;
-  int is_dst;
+  int dst_adjustment;
 
   time_t rawtime;
-  struct tm * timeinfo;
+  struct tm timeinfo;
 
   int tokens_read, max_tokens;
 
@@ -273,22 +273,40 @@ static VALUE parse_date_time(const char *date) {
     }
     // We read the Date and Time, default to the current locale's offset
 
+    tzset();
+
     // Get localtime
     time(&rawtime);
-    timeinfo = localtime(&rawtime);
+    localtime_r(&rawtime, &timeinfo);
 
-    is_dst = timeinfo->tm_isdst * 3600;
+    timeinfo.tm_sec = sec;
+    timeinfo.tm_min = min;
+    timeinfo.tm_hour = hour;
+    timeinfo.tm_mday = day;
+    timeinfo.tm_mon = month;
+    timeinfo.tm_year = year - 1900;
+    timeinfo.tm_isdst = -1;
+
+    // Update tm_isdst
+    mktime(&timeinfo);
+
+    if (timeinfo.tm_isdst) {
+      dst_adjustment = 3600;
+    } else {
+      dst_adjustment = 0;
+    }
 
     // Reset to GM Time
-    timeinfo = gmtime(&rawtime);
+    gmtime_r(&rawtime, &timeinfo);
 
-    gmt_offset = mktime(timeinfo) - rawtime;
+    gmt_offset = rawtime - mktime(&timeinfo);
 
-    if ( is_dst > 0 )
-      gmt_offset -= is_dst;
+    if (dst_adjustment) {
+      gmt_offset += dst_adjustment;
+    }
 
-    hour_offset = -((int)gmt_offset / 3600);
-    minute_offset = -((int)gmt_offset % 3600 / 60);
+    hour_offset = ((int)gmt_offset / 3600);
+    minute_offset = ((int)gmt_offset % 3600 / 60);
 
   } else {
     // Something went terribly wrong

data_objects/lib/data_objects/spec/typecast/datetime_spec.rb
@@ -63,6 +63,32 @@ shared 'supporting DateTime' do
 
     end
 
+    describe 'with manual typecasting a datetime column' do
+
+      before do
+        @command = @connection.create_command("SELECT release_datetime FROM widgets WHERE id = ? OR id = ? ORDER BY id")
+        @command.set_types(DateTime)
+        @reader = @command.execute_reader(1, 10)
+        @reader.next!
+        @feb_row = @reader.values
+        @reader.next!
+        @jul_row = @reader.values
+      end
+
+      after do
+        @reader.close
+      end
+
+      it 'should return the correct offset in Feb' do
+        (@feb_row.first.offset * 86400).to_i.should == Time.local(2008, 2, 14, 0, 31, 12).utc_offset
+      end
+
+      it 'should return the correct offset in Jul' do
+        (@jul_row.first.offset * 86400).to_i.should == Time.local(2008, 7, 14, 0, 31, 12).utc_offset
+      end
+
+    end
+
   end
 
   describe 'writing an DateTime' do

do_derby/spec/spec_helper.rb
@@ -176,6 +176,10 @@ module DataObjectsSpecHelpers
         update widgets set release_timestamp = NULL where id = 9
       EOF
 
+      conn.create_command(<<-EOF).execute_non_query
+        update widgets set release_datetime = '2008-07-14 00:31:12' where id = 10
+      EOF
+
       conn.close
     end
 

do_h2/spec/spec_helper.rb
@@ -155,6 +155,10 @@ module DataObjectsSpecHelpers
         update widgets set release_timestamp = NULL where id = 9
       EOF
 
+      conn.create_command(<<-EOF).execute_non_query
+        update widgets set release_datetime = '2008-07-14 00:31:12' where id = 10
+      EOF
+
       conn.close
     end
 

do_hsqldb/spec/spec_helper.rb
@@ -154,6 +154,10 @@ module DataObjectsSpecHelpers
         update widgets set release_timestamp = NULL where id = 9
       EOF
 
+      conn.create_command(<<-EOF).execute_non_query
+        update widgets set release_datetime = '2008-07-14 00:31:12' where id = 10
+      EOF
+
       ## TODO: change the hexadecimal examples
       conn.close
     end

do_mysql/spec/spec_helper.rb
@@ -141,6 +141,10 @@ module DataObjectsSpecHelpers
       update widgets set release_timestamp = NULL where id = 9
     EOF
 
+    conn.create_command(<<-EOF).execute_non_query
+      update widgets set release_datetime = '2008-07-14 00:31:12' where id = 10
+    EOF
+
     conn.close
 
   end

do_oracle/spec/spec_helper.rb
@@ -181,6 +181,10 @@ module DataObjectsSpecHelpers
       update widgets set release_timestamp = NULL where id = 9
     EOF
 
+    conn.create_command(<<-EOF).execute_non_query
+      update widgets set release_datetime = '2008-07-14 00:31:12' where id = 10
+    EOF
+
     conn.close
     $test_environment_setup_done = true
   end

do_postgres/ext/do_postgres/do_postgres.c
@@ -201,10 +201,10 @@ static VALUE parse_date_time(const char *date) {
   do_int64 num, den;
 
   long int gmt_offset;
-  int is_dst;
+  int dst_adjustment;
 
   time_t rawtime;
-  struct tm * timeinfo;
+  struct tm timeinfo;
 
   int tokens_read, max_tokens;
 
@@ -234,22 +234,40 @@ static VALUE parse_date_time(const char *date) {
     }
     // We read the Date and Time, default to the current locale's offset
 
+    tzset();
+
     // Get localtime
     time(&rawtime);
-    timeinfo = localtime(&rawtime);
+    localtime_r(&rawtime, &timeinfo);
 
-    is_dst = timeinfo->tm_isdst * 3600;
+    timeinfo.tm_sec = sec;
+    timeinfo.tm_min = min;
+    timeinfo.tm_hour = hour;
+    timeinfo.tm_mday = day;
+    timeinfo.tm_mon = month;
+    timeinfo.tm_year = year - 1900;
+    timeinfo.tm_isdst = -1;
+
+    // Update tm_isdst
+    mktime(&timeinfo);
+
+    if (timeinfo.tm_isdst) {
+      dst_adjustment = 3600;
+    } else {
+      dst_adjustment = 0;
+    }
 
     // Reset to GM Time
-    timeinfo = gmtime(&rawtime);
+    gmtime_r(&rawtime, &timeinfo);
 
-    gmt_offset = mktime(timeinfo) - rawtime;
+    gmt_offset = rawtime - mktime(&timeinfo);
 
-    if ( is_dst > 0 )
-      gmt_offset -= is_dst;
+    if (dst_adjustment) {
+      gmt_offset += dst_adjustment;
+    }
 
-    hour_offset = -((int)gmt_offset / 3600);
-    minute_offset = -((int)gmt_offset % 3600 / 60);
+    hour_offset = ((int)gmt_offset / 3600);
+    minute_offset = ((int)gmt_offset % 3600 / 60);
 
   } else {
     // Something went terribly wrong

commit 9e369b79888abd4a3e7b4fd63ac32a38959e7e16

Primarily fix bug introduced in 83d72d60: TZ DST calculation was broken.
Also:

  - moved tzset() into the initialization of the entire library.  There's *no*
    reason to call it every time you parse a date
  - simplified the code a smidge, and named variables better
  - made the code comments more useful

do_mysql/ext/do_mysql/do_mysql.c
@@ -245,8 +245,7 @@ static VALUE parse_date_time(const char *date) {
 
   time_t gmt_offset;
   int dst_adjustment;
-
-  time_t rawtime;
+  time_t current_time;
   struct tm timeinfo;
 
   int tokens_read, max_tokens;
@@ -279,47 +278,38 @@ static VALUE parse_date_time(const char *date) {
       minute_offset = 0;
       sec = 0;
     }
-    // We read the Date and Time, default to the current locale's offset
 
-    tzset();
+    // We read the Date and Time, and we default to the current locale's offset.
 
-    // Get localtime
-    time(&rawtime);
+    // First figure out if we're in DST and set adjustment appropriately.
+
+    time(&current_time);
 #ifdef HAVE_LOCALTIME_R
-    localtime_r(&rawtime, &timeinfo);
+    localtime_r(&current_time, &timeinfo);
 #else
-    timeinfo = *localtime(&rawtime);
+    timeinfo = *localtime(&current_time);
 #endif
 
-    timeinfo.tm_sec = sec;
-    timeinfo.tm_min = min;
-    timeinfo.tm_hour = hour;
-    timeinfo.tm_mday = day;
-    timeinfo.tm_mon = month;
-    timeinfo.tm_year = year - 1900;
+    dst_adjustment = timeinfo.tm_isdst ? 3600 : 0;
+
+    // Now figure out seconds from UTC.  Some modern libc's have tm_gmtoff
+    // in struct tm, but we can't count on that.
+
+    timeinfo.tm_sec   = sec;
+    timeinfo.tm_min   = min;
+    timeinfo.tm_hour  = hour;
+    timeinfo.tm_mday  = day;
+    timeinfo.tm_mon   = month;
+    timeinfo.tm_year  = year - 1900;
     timeinfo.tm_isdst = -1;
 
-    // Update tm_isdst
-    mktime(&timeinfo);
-
-    if (timeinfo.tm_isdst) {
-      dst_adjustment = 3600;
-    } else {
-      dst_adjustment = 0;
-    }
-
-    // Reset to GM Time
 #ifdef HAVE_GMTIME_R
-    gmtime_r(&rawtime, &timeinfo);
+    gmtime_r(&current_time, &timeinfo);
 #else
-    timeinfo = *gmtime(&rawtime);
+    timeinfo = *gmtime(&current_time);
 #endif
 
-    gmt_offset = rawtime - mktime(&timeinfo);
-
-    if (dst_adjustment) {
-      gmt_offset += dst_adjustment;
-    }
+    gmt_offset = current_time - mktime(&timeinfo) + dst_adjustment;
 
     hour_offset = ((int)gmt_offset / 3600);
     minute_offset = ((int)gmt_offset % 3600 / 60);
@@ -1148,4 +1138,6 @@ void Init_do_mysql() {
   for (errs = errors; errs->error_name; errs++) {
     rb_const_set(mMysql, rb_intern(errs->error_name), INT2NUM(errs->error_no));
   }
+
+  tzset();
 }