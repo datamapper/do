05c8f1e4456dc96dad162175b525c81f1be3b39b
Dirkjan Bussink <d.bussink@gmail.com>
2011-11-09 10:05:46 +0100

    Add spec for reading exotic dates
    This spec was broken with the old Date code handling for
    versions < 1.9.3. Since that code has been removed this now
    passes for all versions.

--- do_postgres/spec/typecast/date_spec.rb
+++ do_postgres/spec/typecast/date_spec.rb
@@ -6,4 +6,28 @@ require 'data_objects/spec/shared/typecast/date_spec'
 describe 'DataObjects::Postgres with Date' do
   it_should_behave_like 'supporting Date'
   it_should_behave_like 'supporting Date autocasting'
+
+  describe 'exotic dates' do
+
+    before do
+      @connection = DataObjects::Connection.new(CONFIG.uri)
+      @connection.create_command("INSERT INTO widgets (release_date) VALUES ('0001-01-01')").execute_non_query
+
+      @command = @connection.create_command("SELECT release_date FROM widgets WHERE release_date = '0001-01-01'")
+      @reader = @command.execute_reader
+      @reader.next!
+      @values = @reader.values
+    end
+
+    after do
+      @reader.close
+      @connection.close
+    end
+
+    it 'should return the number of created rows' do
+      @values.first.should == Date.civil(1, 1, 1)
+    end
+
+  end
+
 end
