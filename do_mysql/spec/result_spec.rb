b667220acd4616710406319f3a2c53e6053d0439
Chris Bandy <bandy.chris@gmail.com>
2012-07-12 13:06:51 -0500

    Test insert_id on bigint columns

--- do_mysql/spec/result_spec.rb
+++ do_mysql/spec/result_spec.rb
@@ -15,3 +15,29 @@ describe DataObjects::Mysql::Result do
   it_should_behave_like 'a Result which returns inserted key with sequences'
   it_should_behave_like 'a Result which returns nil without sequences'
 end
+
+describe DataObjects::Mysql::Result do
+
+  describe 'insert_id' do
+
+    before do
+      setup_test_environment
+      @connection = DataObjects::Connection.new(CONFIG.uri)
+      # set the sequence to a value larger than SQL integer
+      command = @connection.create_command('INSERT INTO stuff (id, value) VALUES (?,?)')
+      command.execute_non_query(3_000_000_000, 'cow')
+      # use the sequence to generate an id
+      command = @connection.create_command('INSERT INTO stuff (value) VALUES (?)')
+      @result = command.execute_non_query('monkey')
+    end
+
+    after do
+      @connection.close
+    end
+
+    it 'should return the bigint id' do
+      @result.insert_id.should == 3_000_000_001
+    end
+
+  end
+end
